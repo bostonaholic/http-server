9cd2b9b3e5a12462665d9fc687931a1fc2d41a87
Matthew Boston <matthew@matthewboston.com>
2018-09-05 09:38:49 -0600

    add more HTTP method endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,39 @@ router.get('/', function(req, res, next) {
   res.send('OK');
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
+router.put('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
+router.delete('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
+router.options('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
+router.head('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
+router.trace('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
+router.connect('/', function(req, res, next) {
+  console.log(req.method + ' ' + req.originalUrl);
+  res.send('OK');
+});
+
 module.exports = router;
