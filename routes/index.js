f43255c856669246bcc5e6e3dabf3ca058867947
Matthew Boston <matthew@matthewboston.com>
2018-09-05 18:09:37 -0600

    extract loggers

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,11 @@ const router = require('express').Router();
 const bodyParser = require('body-parser');  // for parsing multipart/form-data
 
 const uuidv1 = require('uuid/v1');
-const util = require('util');
+
+const requestLogger = require('../lib/request_logger');
 
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-const logger = function(msg, obj) {
-  console.log(msg, util.inspect(obj, { showHidden: true,
-                                       depth: Infinity,
-                                       colors: true }));
-};
-
-const requestLogger = function(req, requestId) {
-  const prefix = '[' + requestId + '] ';
-
-  console.log(prefix + req.method + ' ' + req.originalUrl);
-
-  if(!!req.params) {
-    logger(prefix + 'params', req.params);
-  }
-  if(!!req.body) {
-    logger(prefix + 'body', req.body);
-  }
-  if(!!req.query) {
-    logger(prefix + 'query', req.query);
-  }
-};
 
 router.get('/', function(req, res, next) {
   requestLogger(req, uuidv1());
