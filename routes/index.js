b11ecb2beaad6f77ca1591e9395a3d65c895c0ee
Matthew Boston <matthew@matthewboston.com>
2018-09-10 08:39:50 -0600

    log the response

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,27 @@ const bodyParser = require('body-parser');  // for parsing multipart/form-data
 const uuidv1 = require('uuid/v1');
 
 const requestLogger = require('../lib/request_logger');
+const responseLogger = require('../lib/response_logger');
 
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 router.all('/status/*', function(req, res, next) {
-  requestLogger(req, uuidv1());
+  const requestId = uuidv1();
+  requestLogger(req, requestId);
 
   const status = parseInt(req.originalUrl.replace('/status/', ''));
-  res.status(status).send();
+  const response = res.status(status);
+
+  responseLogger(response, requestId);
+  response.send();
 });
 
 router.all('*', function(req, res, next) {
-  requestLogger(req, uuidv1());
+  const requestId = uuidv1();
+  requestLogger(req, requestId);
+
+  responseLogger(res, requestId);
   res.send('OK');
 });
 
