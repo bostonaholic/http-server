8fe31ce9e5de052a78d0a06b60387c9c082006f7
Matthew Boston <matthew@matthewboston.com>
2018-10-12 17:26:49 -0600

    use the standard setup for express application generator

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const server = require('express')();
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
 
-const index = require('./routes/index');
+var indexRouter = require('./routes/index');
 
-server.use('/', index);
+var app = express();
 
-module.exports = server;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
-const router = require('express').Router();
-const bodyParser = require('body-parser');  // for parsing multipart/form-data
+var express = require('express');
+var router = express.Router();
 
 const uuidv1 = require('uuid/v1');
 
 const requestLogger = require('../lib/request_logger');
 const responseLogger = require('../lib/response_logger');
-
-router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 router.all('/respondWith/*', function(req, res, next) {
   const requestId = uuidv1();
