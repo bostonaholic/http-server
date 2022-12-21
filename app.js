810a2cffcfe7e7cf746f87178c7faeb76f3f9f55
Matthew Boston <matthew@matthewboston.com>
2022-12-20 21:49:48 -0700

    linting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
@@ -10,22 +10,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-const requestLogger = require('../lib/request_logger');
-const responseLogger = require('../lib/response_logger');
+const requestLogger = require("../lib/request_logger");
+const responseLogger = require("../lib/response_logger");
 
-router.all('/respondWith/*', function(req, res, next) {
+router.all("/respondWith/*", function (req, res, next) {
   const requestId = uuidv4();
   requestLogger(req, requestId);
 
-  const statusCode = parseInt(req.originalUrl.replace('/respondWith/', ''));
+  const statusCode = parseInt(req.originalUrl.replace("/respondWith/", ""));
   const response = res.status(statusCode);
 
   responseLogger(response, requestId);
   response.send();
 });
 
-router.all('*', function(req, res, next) {
+router.all("*", function (req, res, next) {
   const requestId = uuidv4();
   requestLogger(req, requestId);
 
